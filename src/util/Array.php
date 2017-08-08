48806850293613086f1dab70d4abfe5de154a4cf
Tharindu Perera <tharindudpw@outlook.com>
2017-08-08 13:01:53 +1000

    added empty parameter validation

--- src/util/Array.php
+++ src/util/Array.php
@@ -5,5 +5,5 @@ namespace adityasetiono\util;
 
 function check_params(array $required, ?array $params): bool
 {
-    return is_null($params) ? false : empty(array_diff($required, array_keys($params)));
+    return is_array($params) ? empty(array_diff($required, array_keys($params))) : false;
 }

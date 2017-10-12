2b0bf689f3287fd78dddcf9cc11dd023edbae423
Aditya Setiono <winged.zach@gmail.com>
2017-10-13 10:19:29 +1100

    feature: added filter_params function

--- src/util/Array.php
+++ src/util/Array.php
@@ -7,3 +7,8 @@ function check_params(array $required, ?array $params): bool
 {
     return is_array($params) ? empty(array_diff($required, array_keys($params))) : false;
 }
+
+function filter_params(array $options, array $params)
+{
+    return array_intersect_key($params, array_flip($options));
+}

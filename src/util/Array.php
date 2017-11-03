72950f05583687c49529bd4c32b88e0bed0efb41
Aditya Setiono <winged.zach@gmail.com>
2017-11-03 16:14:50 +1100

    fix: removing ? to support php 7.0

--- src/util/Array.php
+++ src/util/Array.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace adityasetiono\util;
 
-function check_params(array $required, ?array $params): bool
+function check_params(array $required, $params): bool
 {
     return is_array($params) ? empty(array_diff($required, array_keys($params))) : false;
 }
